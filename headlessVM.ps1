3c40b5e82d2aa7b492a9a5af25bf02e11e0df516
jsalmensuo <jesse.salmensuo@outlook.com>
2026-07-06 05:04:05 +0300

    updated PS script to target individual VMs (stopping VMs is unreliable)

--- headlessVM.ps1
+++ headlessVM.ps1
@@ -1,31 +1,33 @@
 # Quick helpers for starting/stopping the k8s lab VMs in VirtualBox.
-# Dot-source this file (or paste into your $PROFILE) to get vbk8s-start,
-# vbk8s-stop and vbk8s-status as commands in any PowerShell window.
-
-$vms = "k8s-master1","k8s-master2","k8s-master3",
-       "k8s-worker1","k8s-worker2","k8s-worker3",
-       "k8s-worker4","k8s-worker5","k8s-worker6"
+$all_vms = "k8s-master1","k8s-master2","k8s-master3",
+           "k8s-worker1","k8s-worker2","k8s-worker3",
+           "k8s-worker4","k8s-worker5","k8s-worker6"
 
 function vbk8s-stop {
-    $vms | ForEach-Object {
+    param([string[]]$target = $all_vms)
+    $target | ForEach-Object {
+        Write-Host "Stopping $_..." -ForegroundColor Yellow
         & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm $_ acpipowerbutton
     }
 }
 
 function vbk8s-start {
-    $vms | ForEach-Object {
+    param([string[]]$target = $all_vms)
+    $target | ForEach-Object {
+        Write-Host "Starting $_..." -ForegroundColor Green
         & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm $_ --type headless
         Start-Sleep -Seconds 10
     }
 }
 
 function vbk8s-status {
+    param([string[]]$target = $all_vms)
     $running = & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" list runningvms
-    $vms | ForEach-Object {
+    $target | ForEach-Object {
         if ($running -match $_) {
-            Write-Host "$_ - running"
+            Write-Host "$_ - running" -ForegroundColor Cyan
         } else {
-            Write-Host "$_ - stopped"
+            Write-Host "$_ - stopped" -ForegroundColor Gray
         }
     }
 }
